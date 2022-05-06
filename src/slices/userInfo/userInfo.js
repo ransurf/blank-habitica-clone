2368e65177fdd15783f854bd54bcf0c91ddb615a
ransurf <ransomguest@gmail.com>
2022-05-05 22:03:32 -0700

    background + app fill whole screen

--- src/slices/userInfo/userInfo.js
+++ src/slices/userInfo/userInfo.js
@@ -1,29 +1,23 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 export const userIDSlice = createSlice({
-    name: 'userInfo',
-    initialState: {
-        userID: '',
-        apiKey: '',
+  name: "userInfo",
+  initialState: {
+    userID: "",
+    apiKey: "",
+  },
+  reducers: {
+    setUserID: (state, action) => {
+      //state is current state, payload is the userID
+      state.userID = action.payload;
     },
-    reducers: {
-        setUserID: (state, action) => {
-            //state is current state, payload is the userID
-            
-            console.log('setUserID payload: ', action.payload);
-            state.userID = action.payload;
-            console.log('setUserID state: ', state);
-        },
-        setApiKey: (state, action) => {
-            //state is current state, payload is the apiKey
-            state.apiKey = action.payload;
-        }
-    }
-}
-);
+    setApiKey: (state, action) => {
+      //state is current state, payload is the apiKey
+      state.apiKey = action.payload;
+    },
+  },
+});
 
 export const { setUserID, setApiKey } = userIDSlice.actions;
-
-
 
 export default userIDSlice.reducer;
